2837d7b69b79c7d8fd83b5bab629605397fae7af
Nicolas Thiébaud <ni.thiebaud@gmail.com>
2011-09-10 17:33:37 +0200

    projet avec utilisateur

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seednetworking::Application.config.secret_token = 'a96b163faa4dbc05dcff1b512d2f843595db7ae270442bc414a4494ac9bea0b2d9f2597a4d0e772bcd34143797e79cb99d3b74ff022a9968e8b6abd8426edcea'
